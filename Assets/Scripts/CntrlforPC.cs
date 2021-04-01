f03f2994961ce0dc4918144aeb0035a9c22f25c3
Yaroslav Moisiuk-Dranko <jajaroslove@gmail.com>
2021-04-01 11:52:53 +0300

    Add Files

---
+++ Assets/Scripts/CntrlforPC.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private bool faceRight = true;
+    
+    public float speed = 20f; // player speed
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        //Run
+        float moveX = Input.GetAxis("Horizontal");// player direction
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector2.up * 8000);
+        //Flip player
+        if (moveX > 0 && !faceRight) 
+            flip ();
+        else if(moveX < 0 && faceRight)
+        flip ();
+    }
+
+    void flip()
+    {
+        faceRight = !faceRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
